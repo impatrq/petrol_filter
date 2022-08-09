746f60bf4049c9dea14550f305548bd9921eee16
Guido Vilardo <78625995+guidovilardo@users.noreply.github.com>
2022-08-08 22:31:16 -0300

    Add files via upload
    las funciones de los scripts están importadas en el programa central (main.py), en el script notifications.py funciona el bot de envio de notificaciones y demás datos.

---
+++ bounce_filter.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#este algoritmo lo que hace es acumular N valores recientes, descarta picos y calcula el promedio de las mediciones restantes
+#evitando que las lecturas del sensor fluctuen por el rebote con el agua
+from collections import deque
+
+class BounceFilter(object):
+    def __init__(self, size=10, discard_count=2):
+        self.__size = size
+        self.__discard_count = discard_count
+
+        self.__data = deque(maxlen=size)
+
+    @staticmethod
+    def discard(data, discard_count):
+        n = discard_count
+        if len(data) <= n * 2:
+            return data
+        return data[n:-n]
+
+    @staticmethod
+    def extract_sorted_digits(data):
+        d = list(n for n in data if n is not None)
+        d.sort()
+        return d
+
+    def add(self, value):
+        self.__data.extendleft([value])
+
+    def get_all(self):
+        return list(self.__data)
+
+    def avg(self):
+        digits = self.extract_sorted_digits(self.__data)
+        values = self.discard(digits, self.__discard_count)
+        if values:
+            return sum(values) / len(values)
+        return None
